a2193fc37700b7e9dedf5aed423bfb62a74aeae1
MauricioSts <Mauricio.santos.110@ufrn.edu.br>
2025-04-08 13:29:33 -0300

    iniciando o projeto e criando a interface

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:app_notas_diarias/Home.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+  debugShowCheckedModeBanner: false,
+));
